4fe372ccf8f47824814ece0c60e73d442cf3a51e
Yoonjae Cho <yjcho97@postech.ac.kr>
2024-05-22 13:53:18 +0900

    Modified utils.py

--- utils.py
+++ utils.py
@@ -1,21 +1,7 @@
 import numpy as np
 
 class EarlyStopping:
-    """Early stops the training if validation loss doesn't improve after a given patience."""
     def __init__(self, patience=7, verbose=False, delta=0, trace_func=print):
-        """
-        Args:
-            patience (int): How long to wait after last time validation loss improved.
-                            Default: 7
-            verbose (bool): If True, prints a message for each validation loss improvement. 
-                            Default: False
-            delta (float): Minimum change in the monitored quantity to qualify as an improvement.
-                            Default: 0
-            path (str): Path for the checkpoint to be saved to.
-                            Default: 'checkpoint.pt'
-            trace_func (function): trace print function.
-                            Default: print            
-        """
         self.patience = patience
         self.verbose = verbose
         self.counter = 0
