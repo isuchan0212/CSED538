f64b916c05e1699fa3b9470448e98d327d123b4c
Jongmin Park <jmpp21@postech.ac.kr>
2024-05-23 20:25:56 +0900

    수정

--- main.py
+++ main.py
@@ -1,107 +1,65 @@
-# import torch
-# import torchvision
-# import torchvision.transforms as transforms
-# import torch.optim as optim
-# from model import Net
-# from utils import train, test, imshow, classes, adjust_learning_rate
-
-# def main():
-#     # 데이터 로드 및 전처리
-#     transform = transforms.Compose(
-#         [transforms.RandomHorizontalFlip(),
-#          transforms.RandomCrop(32, padding=4),
-#          transforms.ToTensor(),
-#          transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
-
-#     batch_size = 4
-
-#     trainset = torchvision.datasets.CIFAR10(root='./data', train=True,
-#                                             download=True, transform=transform)
-#     trainloader = torch.utils.data.DataLoader(trainset, batch_size=batch_size,
-#                                               shuffle=True, num_workers=2)
-
-#     testset = torchvision.datasets.CIFAR10(root='./data', train=False,
-#                                            download=True, transform=transforms.Compose([
-#                                                transforms.ToTensor(),
-#                                                transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
-#                                            ]))
-#     testloader = torch.utils.data.DataLoader(testset, batch_size=batch_size,
-#                                              shuffle=False, num_workers=2)
-
-#     net = Net()
-
-#     # 손실 함수 및 옵티마이저 정의
-#     criterion = torch.nn.CrossEntropyLoss()
-#     initial_lr = 0.01
-#     optimizer = optim.SGD(net.parameters(), lr=initial_lr, momentum=0.9, weight_decay=5e-4)
-#     scheduler = optim.lr_scheduler.StepLR(optimizer, step_size=5, gamma=0.1)
-
-#     # 모델 학습
-#     num_epochs = 20
-#     for epoch in range(num_epochs):  # 20 에포크 동안 학습
-#         train(net, trainloader, criterion, optimizer, epoch)
-#         scheduler.step()
-
-#     print('Finished Training')
-
-#     # 모델 저장
-#     PATH = './cifar_net.pth'
-#     torch.save(net.state_dict(), PATH)
-
-#     # 학습 결과 시각화
-#     dataiter = iter(testloader)
-#     images, labels = next(dataiter)
-
-#     imshow(torchvision.utils.make_grid(images))
-#     print('GroundTruth: ', ' '.join('%5s' % classes[labels[j]] for j in range(4)))
-
-#     # 모델 불러오기
-#     net = Net()
-#     net.load_state_dict(torch.load(PATH))
-
-#     # 테스트 데이터셋으로 모델 평가
-#     test(net, testloader)
-
-# if __name__ == "__main__":
-#     main()
+import numpy as np
+import random
 
 import torch
 import torchvision.transforms as transforms
 from torch.utils.data import DataLoader, random_split
+import torch.nn as nn
 import torch.optim as optim
-from model import SimpleVGG
+
 from dataset import get_cifar10_dataset
 from train_val_test import train_val, test
+from model import Net
 
-# Transformations for data augmentation
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-s', '--lr_scheduler')
+parser.add_argument('-a', '--adaptive_scheduling',action='store_true')
+
+args = parser.parse_args()
+
+random_seed = 42 
+
+generator = torch.Generator().manual_seed(random_seed)
+torch.manual_seed(random_seed)
+torch.cuda.manual_seed(random_seed)
+torch.backends.cudnn.deterministic = True
+torch.backends.cudnn.benchmark = False
+np.random.seed(random_seed)
+random.seed(random_seed)
+
 transform = transforms.Compose(
-    [transforms.RandomHorizontalFlip(),
-     transforms.RandomCrop(32, padding=4),
-     transforms.ToTensor(),
-     transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))]
+    [transforms.ToTensor()]
 )
-generator = torch.Generator().manual_seed(42)
 
-model = SimpleVGG()
+model = Net()
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
-print(f'Using device: {device}')  # 디바이스 정보 출력
-model.to(device)  # 모델을 GPU로 전송
+num_epochs = 200
 
-num_epochs = 20
-
-dataset = get_cifar10_dataset(root='./data', train=True, download=True, transform=transform)
-test_set = get_cifar10_dataset(root='./data', train=False, download=True, transform=transform)
+dataset = get_cifar10_dataset(root='./data', train=True, download=False, transform=transform)
+test_set = get_cifar10_dataset(root='./data', train=False, download=False, transform=transform)
 
 train_set, val_set = random_split(dataset, [0.8, 0.2], generator=generator)
 
-train_loader = DataLoader(train_set, batch_size=64, shuffle=True, num_workers=4, pin_memory=True)
-val_loader = DataLoader(val_set, batch_size=64, shuffle=False, num_workers=4, pin_memory=True)
-test_loader = DataLoader(test_set, batch_size=64, shuffle=False, num_workers=4, pin_memory=True)
 
-criterion = torch.nn.CrossEntropyLoss()
-optimizer = optim.SGD(model.parameters(), lr=0.01, momentum=0.9, weight_decay=5e-4)
+train_loader = DataLoader(train_set, batch_size = 128, shuffle=True, num_workers=4, pin_memory=True)
+val_loader = DataLoader(val_set, batch_size = 128, shuffle=False, num_workers=4, pin_memory=True)
+test_loader = DataLoader(test_set, batch_size = 128, shuffle=False, num_workers=4, pin_memory=True)
+
+
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.SGD(model.parameters(), lr=1e-3, momentum=0.9) if not args.adaptive_scheduling else optim.Adam(model.parameters(), lr=1e-3, amsgrad=True)
+
+schedulers = {
+    'step': optim.lr_scheduler.StepLR(optimizer,50),
+    'exponential': optim.lr_scheduler.ExponentialLR(optimizer,0.998),
+    'polynomial': optim.lr_scheduler.PolynomialLR(optimizer, num_epochs),
+    'cosine': optim.lr_scheduler.CosineAnnealingLR(optimizer,50),
+    'cosinewarmup': optim.lr_scheduler.CosineAnnealingWarmRestarts(optimizer,50,T_mult=2) 
+    }
+scheduler = schedulers[args.lr_scheduler] if args.lr_scheduler else None
 
 if __name__ == '__main__':
-    print("Training started.")
-    train_val(model, device, num_epochs, train_loader, val_loader, criterion, optimizer)
+    train_val(model, device, num_epochs, train_loader,val_loader, criterion, optimizer, scheduler)
     test(model, device, test_loader)
--- train_val_test.py
+++ train_val_test.py
@@ -1,51 +1,62 @@
 import torch
 
-def train_val(model, device, num_epochs, train_loader, val_loader, criterion, optimizer):
+def train_val(model, device, num_epochs, train_loader,val_loader, criterion, optimizer, scheduler=None):
     torch.multiprocessing.freeze_support()
     model.to(device)
 
+    criterion = criterion
+    optimizer = optimizer
+    scheduler = scheduler
+
     for epoch in range(num_epochs):
-        # Training phase
-        model.train()
+        model.train()  # Set the model to training mode
         running_loss = 0.0
         for inputs, targets in train_loader:
-            inputs, targets = inputs.to(device), targets.to(device)  # 데이터를 GPU로 전송
-            # print(f'Training on device: {inputs.device}')  # 입력 데이터 디바이스 정보 출력
+            inputs, targets = inputs.to(device), targets.to(device)
 
             optimizer.zero_grad()
+
             outputs = model(inputs)
+
             loss = criterion(outputs, targets)
+
             loss.backward()
             optimizer.step()
-
+            
             running_loss += loss.item() * inputs.size(0)
 
-        train_loss = running_loss / len(train_loader.dataset)
-        print(f'Epoch {epoch+1}, Train Loss: {train_loss:.2f}')
+        epoch_loss = running_loss / len(train_loader.dataset)
+        print(f'Epoch {epoch+1}, Train Loss: {epoch_loss}')
+
+        if scheduler is not None:
+            scheduler.step()
 
         # Validation phase
-        model.eval()
-        val_running_loss = 0.0
+        model.eval()  # Set the model to evaluation mode
         with torch.no_grad():
+            val_running_loss = 0.0
             for inputs, targets in val_loader:
-                inputs, targets = inputs.to(device), targets.to(device)  # 데이터를 GPU로 전송
-                # print(f'Validation on device: {inputs.device}')  # 입력 데이터 디바이스 정보 출력
+                inputs, targets = inputs.to(device), targets.to(device)
+                
                 outputs = model(inputs)
+                
                 loss = criterion(outputs, targets)
+
                 val_running_loss += loss.item() * inputs.size(0)
 
-        val_loss = val_running_loss / len(val_loader.dataset)
-        print(f'Epoch {epoch+1}, Validation Loss: {val_loss:.2f}')
-            
+            val_epoch_loss = val_running_loss / len(val_loader.dataset)
+            print(f'Epoch {epoch+1}, Validation Loss: {val_epoch_loss}')
+
 
 def test(model, device, test_loader):
+
     model.eval()
     correct = 0
     total = 0
     with torch.no_grad():
         for inputs, targets in test_loader:
-            inputs, targets = inputs.to(device), targets.to(device)  # 데이터를 GPU로 전송
-            # print(f'Testing on device: {inputs.device}')  # 입력 데이터 디바이스 정보 출력
+            inputs, targets = inputs.to(device), targets.to(device)
+            
             outputs = model(inputs)
             _, predicted = torch.max(outputs.data, 1)
             total += targets.size(0)
